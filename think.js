a181016edf5e699d822734d1cbb23e32691d656e
Tanner Lemon <48069584+tannerlemon@users.noreply.github.com>
2020-10-15 11:51:02 -0400

    complete

---
+++ think.js
@@ -0,0 +1,25 @@
+// let letters from someone you know equals friend or family
+
+// let letters from the govt. or well-designed letters equals looks important
+
+// let letters from bill providers and debt collectors equals bill
+
+ 
+
+// for each letter
+
+//  if it's from a friend or family member or otherwise looks important
+
+//      open it and read it
+
+//  if it's a bill
+
+//      open it, read it, and throw it away because you're poor and can't afford that right now
+
+//  if it's a check
+
+//      open it and deposit the check
+
+//  else
+
+//      mark it as junk and throw it away
